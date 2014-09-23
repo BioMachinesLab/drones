648f0b3df1e32a713b05f6d5212c6134c81d9d6e
atsoc0ocsav@gmail.com <atsoc0ocsav@gmail.com@f2965c81-1508-54b6-3f6b-0b5a321ce6b6>
2014-09-23 11:09:37 +0000

    Added the network messages source again

---
+++ DroneControlConsole/src/network/messages/SystemInformationsMessage.java
@@ -0,0 +1,17 @@
+package network.messages;
+
+import dataObjects.SystemInformationsData;
+
+public class SystemInformationsMessage extends Message {
+	private static final long serialVersionUID = -8333006310686657108L;
+	private SystemInformationsData sysInformations;
+
+	public SystemInformationsMessage(SystemInformationsData sysInformations) {
+		super();
+		this.sysInformations = sysInformations;
+	}
+
+	public SystemInformationsData getSysInformations() {
+		return sysInformations;
+	}
+}
