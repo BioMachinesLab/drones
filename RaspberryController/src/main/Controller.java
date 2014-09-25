ea62fe7e5d7318e6b3d2f2a553b5dd73b3c801e3
atsoc0ocsav@gmail.com <atsoc0ocsav@gmail.com@f2965c81-1508-54b6-3f6b-0b5a321ce6b6>
2014-09-25 02:07:09 +0000

    Moved the IOException from the connectionHandler to the controller

--- RaspberryController/src/main/Controller.java
+++ RaspberryController/src/main/Controller.java
@@ -1,8 +1,8 @@
 package main;
 
-import input.BatteryManagerInput;
-import input.CompassModuleInput;
-import input.GPSModuleInput;
+import io.UnavailableDeviceException;
+import io.input.GPSModuleInput;
+import io.output.ESCManagerOutputThreadedImprov;
 
 import java.io.IOException;
 import java.text.ParseException;
@@ -14,7 +14,7 @@ import network.messages.InformationRequest;
 import network.messages.Message;
 import network.messages.MotorMessage;
 import network.messages.SystemInformationsMessage;
-import output.ESCManagerOutputThreaded;
+import network.messages.SystemStatusMessage;
 
 import com.pi4j.io.serial.SerialPortException;
 
@@ -22,10 +22,9 @@ import dataObjects.SystemInformationsData;
 
 public class Controller {
 	private GPSModuleInput gpsModule;
-	private BatteryManagerInput batteryManager;
-	private ESCManagerOutputThreaded escManagerThreaded;
-	private CompassModuleInput compassModule;
+	private ESCManagerOutputThreadedImprov escManagerThreaded;
 	private ConnectionHandler networkConnector;
+	private String messages = "";
 
 	public static void main(String[] args) throws SerialPortException {
 		new Controller();
@@ -34,24 +33,49 @@ public class Controller {
 	public Controller() {
 		Runtime.getRuntime().addShutdownHook(new Thread() {
 			public void run() {
+				System.out.println("# Shutting down... ");
 				if (networkConnector != null) {
 					networkConnector.closeConnections();
+				}
+				if (escManagerThreaded != null) {
 					escManagerThreaded.disableMotors();
 				}
+				if (gpsModule != null) {
+					gpsModule.closeSerial();
+				}
+				System.out.println("now!");
 			}
 		});
 
 		System.out.println("######################################");
-		System.out.print("Initializing ESC modules");
-		escManagerThreaded = new ESCManagerOutputThreaded();
-		escManagerThreaded.start();
+		System.out.println("Initializing...");
+		try {
+			escManagerThreaded = new ESCManagerOutputThreadedImprov();
+			escManagerThreaded.start();
+			System.out.println("# ESC modules initialized with success!");
+		} catch (UnavailableDeviceException e) {
+			System.out.println("Unable to start ESC modules!");
+			messages += "Unable to start ESC module!\n";
+			e.printStackTrace();
+		}
 
-		System.out.print(", GPS module");
-		gpsModule = new GPSModuleInput();
+		try {
+			gpsModule = new GPSModuleInput();
+			System.out.println("# GPS Module initialized with success!");
+		} catch (UnavailableDeviceException e) {
+			System.out.println("Unable to start GPS module!");
+			messages += "Unable to start GPS module!\n";
+		}
 
-		System.out.println(", network connections");
-		networkConnector = new ConnectionHandler(this);
-		networkConnector.initConnector();
+		try {
+			networkConnector = new ConnectionHandler(this);
+			networkConnector.initConnector();
+			System.out
+					.println("# Network Connection initialized with success!");
+		} catch (IOException e) {
+			System.out.println("Unable to start Netwok Connector!");
+			messages += "Unable to start Network Connector!\n";
+		}
 
 		// batteryManager = new BatteryManagerInput();
 		// compassModule = new CompassModuleInput();
@@ -75,23 +99,43 @@ public class Controller {
 			msg = null;
 			break;
 		case GPS:
-			msg = new GPSMessage(gpsModule.getReadings());
+			if (gpsModule != null) {
+				msg = new GPSMessage(gpsModule.getReadings());
+			} else {
+				msg = new SystemStatusMessage("Unable to send GPS data");
+			}
+			conn.sendData(msg);
 			break;
-		case SYSTEM:
+		case SYSTEM_INFO:
 			try {
 				msg = new SystemInformationsMessage(
 						new SystemInformationsData());
 			} catch (IOException | InterruptedException | ParseException e) {
 				System.err.println("Error fetching informations from system!");
-				msg = null;
-				e.printStackTrace();
+				msg = new SystemStatusMessage(
+						"Error fetching informations from system!");
+			}
+			conn.sendData(msg);
+			break;
+		case SYSTEM_STATUS:
+			if (messages != null) {
+				msg = new SystemStatusMessage(messages);
+				System.out
+						.println("[CONTROLLER)] I sent messages: " + messages);
+				messages = null;
+				conn.sendData(msg);
 			}
 			break;
 		default:
 			msg = null;
 			break;
 		}
+	}
 
-		conn.sendData(msg);
+	public synchronized void sendMessageToOperator(String message) {
+		if (messages == null)
+			messages = message + "\n";
+		else
+			messages += message + "\n";
 	}
 }
--- RaspberryController/src/network/ConnectionHandler.java
+++ RaspberryController/src/network/ConnectionHandler.java
@@ -17,35 +17,33 @@ public class ConnectionHandler {
 		this.controller = controller;
 	}
 
-	public void initConnector() {
-		ServerSocket serverSocket;
-		try {
-			serverSocket = new ServerSocket(SOCKET_PORT);
+	public void initConnector() throws IOException {
+		ServerSocket serverSocket = new ServerSocket(SOCKET_PORT);
 
-			try {
-				System.out.println("[CONNECTION HANDLER] Connection Handler Initialized on "
-						+ InetAddress.getLocalHost().getHostAddress() + ":"
-						+ SOCKET_PORT);
-				System.out.println("[CONNECTION HANDLER] Waiting for connection requests!");
-				while (true) {
-					Socket socket = serverSocket.accept();
-					Connection conn = new Connection(socket, controller, this);
-					connections.add(conn);
-					conn.start();
-				}
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} finally {
-				try {
-					serverSocket.close();
-				} catch (IOException e) {
-					System.out
-							.println("[CONNECTION HANDLER] Unable to close server socket.... there was an open socket?");
-				}
+		try {
+			System.out
+					.println("[CONNECTION HANDLER] Connection Handler Initialized on "
+							+ InetAddress.getLocalHost().getHostAddress()
+							+ ":"
+							+ SOCKET_PORT);
+			System.out
+					.println("[CONNECTION HANDLER] Waiting for connection requests!");
+			while (true) {
+				Socket socket = serverSocket.accept();
+				Connection conn = new Connection(socket, controller, this);
+				connections.add(conn);
+				conn.start();
 			}
 		} catch (IOException e) {
-			System.out.println("[CONNECTION HANDLER] Unable to init socket!");
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				serverSocket.close();
+			} catch (IOException e) {
+				System.out
+						.println("[CONNECTION HANDLER] Unable to close server socket.... there was an open socket?");
+			}
 		}
 	}
 
