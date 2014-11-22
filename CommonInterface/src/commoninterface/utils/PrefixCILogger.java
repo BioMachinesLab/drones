97084578526a4af31fcb45614e235fabae89c604
anderslyhne@gmail.com <anderslyhne@gmail.com@f2965c81-1508-54b6-3f6b-0b5a321ce6b6>
2014-11-22 14:32:05 +0000

    Moved log prefixing into a separate class and added a SpyCI.

---
+++ CommonInterface/src/commoninterface/utils/PrefixCILogger.java
@@ -0,0 +1,33 @@
+package commoninterface.utils;
+
+import commoninterface.CILogger;
+
+/**
+ * A logger able to prefix other loggers.
+ * 
+ * @author alc
+ */
+public class PrefixCILogger implements CILogger {
+	private String prefix = "";
+	private CILogger originalLogger;
+	
+	public PrefixCILogger(CILogger originalLogger, String prefix) {
+		this.originalLogger = originalLogger;
+		this.prefix         = prefix;
+	}
+	
+	public void setPrefix(String prefix) {
+		this.prefix = prefix;
+	}
+	
+	@Override
+	public void logMessage(String message) {
+		originalLogger.logMessage(prefix + ": " + message);
+	}
+
+	@Override
+	public void logError(String error) {
+		originalLogger.logMessage(prefix + ": " + error);		
+	}
+
+}
