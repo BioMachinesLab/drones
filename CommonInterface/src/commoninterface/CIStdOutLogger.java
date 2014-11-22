c3b8a597c82d48f1d8fb9ebf7e5460a6293c32a3
anderslyhne@gmail.com <anderslyhne@gmail.com@f2965c81-1508-54b6-3f6b-0b5a321ce6b6>
2014-11-22 12:33:06 +0000

    Added optional prefix to log and error messages.

--- CommonInterface/src/commoninterface/CIStdOutLogger.java
+++ CommonInterface/src/commoninterface/CIStdOutLogger.java
@@ -3,19 +3,24 @@ package commoninterface;
 public class CIStdOutLogger implements CILogger 
 {	
 	private AquaticDroneCI droneCI;
+	private String prefix = "";
 	
 	public CIStdOutLogger(AquaticDroneCI droneCI) {
 		this.droneCI = droneCI;
 	}
 	
+	public void setPrefix(String prefix) {
+		this.prefix = prefix;
+	}
+	
 	@Override
 	public void logMessage(String message) {
-		System.out.printf("%6.2s: %s\n", droneCI.getTimeSinceStart(), message);
+		System.out.printf("%s %6.2s: %s\n", prefix, droneCI.getTimeSinceStart(), message);
 	}
 
 	@Override
 	public void logError(String error) {
-		System.err.printf("%6.2s: %s\n", droneCI.getTimeSinceStart(), error);
+		System.err.printf("%s %6.2s: %s\n", prefix, droneCI.getTimeSinceStart(), error);
 	}
 	
 }
