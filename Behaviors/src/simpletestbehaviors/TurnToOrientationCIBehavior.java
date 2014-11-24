9c17ac756493d2ed112e4b92fee15234ada3c2ba
miguelduarte42 <miguelduarte42@f2965c81-1508-54b6-3f6b-0b5a321ce6b6>
2014-11-24 13:38:06 +0000

    Slight modification of the CI

--- Behaviors/src/simpletestbehaviors/TurnToOrientationCIBehavior.java
+++ Behaviors/src/simpletestbehaviors/TurnToOrientationCIBehavior.java
@@ -9,8 +9,7 @@ public class TurnToOrientationCIBehavior extends CIBehavior {
 	private double targetOrientation = 0;
 	private double tolerance         = 20;
 	
-	public TurnToOrientationCIBehavior(String[] args, AquaticDroneCI drone,
-			CILogger logger) {
+	public TurnToOrientationCIBehavior(String[] args, AquaticDroneCI drone, CILogger logger) {
 		super(args, drone, logger);
 		
 		//TODO: Find a more elegant way to parse arguments
@@ -29,24 +28,30 @@ public class TurnToOrientationCIBehavior extends CIBehavior {
 	@Override
 	public void step() {
 		double currentOrientation = drone.getCompassOrientationInDegrees();		
-		getLogger().logMessage("Current orientation: " + currentOrientation);
+//		getLogger().logMessage("Current orientation: " + currentOrientation);
 		double difference = currentOrientation - targetOrientation;
-		getLogger().logMessage("Current difference: " + difference);
+//		getLogger().logMessage("Current difference: " + difference);
 	
 		if (Math.abs(difference) <= tolerance) {
 			drone.setLed(0, AquaticDroneCI.LedState.ON);
 			drone.setMotorSpeeds(0, 0);
-			getLogger().logMessage("Within tolerance, stopping");
+//			getLogger().logMessage("Within tolerance, stopping");
 		}
 		else {
 			drone.setLed(0, AquaticDroneCI.LedState.BLINKING);
 			if (difference < 180) {
-				getLogger().logMessage("Turning right");
+//				getLogger().logMessage("Turning right");
 				drone.setMotorSpeeds(0.1, -0.1);
 			} else {
-				getLogger().logMessage("Turning left");				
+//				getLogger().logMessage("Turning left");				
 				drone.setMotorSpeeds(-0.1, 0.1);
 			}
 		}
+	}
+	
+	@Override
+	public void cleanUp() {
+		drone.setLed(0, AquaticDroneCI.LedState.OFF);
+		drone.setMotorSpeeds(0, 0);
 	}
 }
