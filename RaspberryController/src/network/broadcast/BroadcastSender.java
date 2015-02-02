1edec101147afc0a58d9ffbd709622bd3971c1d5
miguelduarte42 <miguelduarte42@f2965c81-1508-54b6-3f6b-0b5a321ce6b6>
2015-02-02 14:34:55 +0000

    Changed the logic of BroadcastHandler. It now depends on a general one from CommonInterface

---
+++ RaspberryController/src/network/broadcast/BroadcastSender.java
@@ -0,0 +1,34 @@
+package network.broadcast;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class BroadcastSender {
+
+	private DatagramSocket socket;
+	private int port;
+
+	public BroadcastSender(String ownAddress, int port) {
+		this.port = port;
+		try {
+			socket = new DatagramSocket(port, InetAddress.getByName(ownAddress));
+			socket.setBroadcast(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void sendMessage(String message) {
+		try {
+			byte[] sendData = message.getBytes();
+			DatagramPacket sendPacket = new DatagramPacket(sendData,
+					sendData.length, InetAddress.getByName("255.255.255.255"),
+					port);
+			socket.send(sendPacket);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
