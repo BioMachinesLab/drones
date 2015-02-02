62b90880a41d58e662b58f42ac51417581e4b6cb
miguelduarte42 <miguelduarte42@f2965c81-1508-54b6-3f6b-0b5a321ce6b6>
2015-02-02 14:33:56 +0000

    Changed the logic of BroadcastHandler. It now depends on a general one from CommonInterface

---
+++ RaspberryController/src/network/broadcast/BroadcastReceiver.java
@@ -0,0 +1,47 @@
+package network.broadcast;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import commoninterface.network.broadcast.BroadcastHandler;
+
+public class BroadcastReceiver extends Thread{
+	
+	private static int BUFFER_LENGTH = 15000;
+	private DatagramSocket socket;
+	private Thread receiver;
+	private String ownAddress;
+	private BroadcastHandler bh;
+
+	public BroadcastReceiver(BroadcastHandler bh, String ownAddress, int port) {
+		this.ownAddress = ownAddress;
+		this.bh = bh;
+		try {
+			 socket = new DatagramSocket(port, InetAddress.getByName("0.0.0.0"));
+			 socket.setBroadcast(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		receiver.start();
+	}
+
+	@Override
+	public void run() {
+
+		try {
+			while (true) {
+				byte[] recvBuf = new byte[BUFFER_LENGTH];
+				DatagramPacket packet = new DatagramPacket(recvBuf,recvBuf.length);
+				socket.receive(packet);
+
+				String message = new String(packet.getData()).trim();
+				
+				if(!packet.getAddress().getHostAddress().equals(ownAddress))
+					bh.messageReceived(packet.getAddress().getHostAddress(), message);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
