9e9f3683418c20fcedd7bf0d70a16448bf46181f
tiagorodrigues11 <tiagorodrigues11@gmail.com>
2015-05-09 01:26:00 +0100

    Added a synchronized to the robot entities in the Virtual Position Broadcast Message Message and Message Provided added for the Thymio Virtual Position Message

---
+++ CommonInterface/src/commoninterface/messageproviders/ThymioVirtualPositionMessageProvider.java
@@ -0,0 +1,33 @@
+package commoninterface.messageproviders;
+
+import commoninterface.ThymioCI;
+import commoninterface.mathutils.Vector2d;
+import commoninterface.network.messages.InformationRequest;
+import commoninterface.network.messages.Message;
+import commoninterface.network.messages.MessageProvider;
+import commoninterface.network.messages.ThymioVirtualPositionMessage;
+
+public class ThymioVirtualPositionMessageProvider implements MessageProvider {
+
+	private ThymioCI thymio;
+	private Vector2d unknownPosition;
+	private double unknownOrientation;
+	
+	public ThymioVirtualPositionMessageProvider(ThymioCI thymio) {
+		this.thymio = thymio;
+		unknownPosition = new Vector2d(-1, -1);
+		unknownOrientation = 0;
+	}
+	
+	@Override
+	public Message getMessage(Message request) {
+		if(request instanceof InformationRequest && ((InformationRequest)request).getMessageTypeQuery().equals(InformationRequest.MessageType.THYMIO_VIRTUAL_POSITION)){
+			if(thymio.getVirtualPosition() != null)
+				return new ThymioVirtualPositionMessage(thymio.getVirtualPosition(), thymio.getVirtualOrientation());
+			else
+				return new ThymioVirtualPositionMessage(unknownPosition, unknownOrientation);
+		}
+		return null;
+	}
+
+}
---
+++ CommonInterface/src/commoninterface/network/messages/ThymioVirtualPositionMessage.java
@@ -0,0 +1,24 @@
+package commoninterface.network.messages;
+
+import commoninterface.mathutils.Vector2d;
+
+public class ThymioVirtualPositionMessage extends Message {
+
+	private static final long serialVersionUID = -398651655354216523L;
+	private Vector2d virtualThymioPosition;
+	private double virtualOrientation;
+	
+	public ThymioVirtualPositionMessage(Vector2d virtualThymioPosition, double virtualOrientation) {
+		this.virtualThymioPosition = virtualThymioPosition;
+		this.virtualOrientation = virtualOrientation;
+	}
+	
+	public Vector2d getVirtualThymioPosition() {
+		return virtualThymioPosition;
+	}
+	
+	public double getVirtualOrientation() {
+		return virtualOrientation;
+	}
+
+}
