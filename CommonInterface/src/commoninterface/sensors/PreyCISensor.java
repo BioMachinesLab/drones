d7045779766d83f740d1df193903ad3726bf0c1c
tiagorodrigues11 <tiagorodrigues11@gmail.com>
2015-05-15 02:15:11 +0100

    Implemented Thymio Sensor Sharing

--- CommonInterface/src/commoninterface/sensors/PreyCISensor.java
+++ CommonInterface/src/commoninterface/sensors/PreyCISensor.java
@@ -1,19 +1,33 @@
 package commoninterface.sensors;
 
 import commoninterface.RobotCI;
-import commoninterface.objects.Entity;
 import commoninterface.objects.PreyEntity;
+import commoninterface.objects.ThymioSharedEntity;
+import commoninterface.objects.VirtualEntity;
 import commoninterface.utils.CIArguments;
 
 public class PreyCISensor extends ThymioConeTypeCISensor {
 
+	private boolean share = false;
+	
 	public PreyCISensor(int id, RobotCI robot, CIArguments args) {
 		super(id, robot, args);
+		share = args.getFlagIsTrue("share");
 	}
 
 	@Override
 	public boolean validEntity(Object e) {
 		return e instanceof PreyEntity;
 	}
-
+	
+	@Override
+	protected void sensedEntity(VirtualEntity ve) {
+		if(share && ve instanceof PreyEntity) {
+			PreyEntity pe = (PreyEntity)ve;
+			ThymioSharedEntity tse = new ThymioSharedEntity(pe.getName(), robot.getNetworkAddress(), pe.getPosition());
+			tse.setTimestepReceived((long)(robot.getTimeSinceStart()*10));
+			robot.replaceEntity(tse);
+		}
+	}
+	
 }
---
+++ CommonInterface/src/commoninterface/sensors/SharedThymioCISensor.java
@@ -0,0 +1,18 @@
+package commoninterface.sensors;
+
+import commoninterface.RobotCI;
+import commoninterface.objects.ThymioSharedEntity;
+import commoninterface.utils.CIArguments;
+
+public class SharedThymioCISensor extends ThymioConeTypeCISensor {
+
+	public SharedThymioCISensor(int id, RobotCI robot, CIArguments args) {
+		super(id, robot, args);
+	}
+
+	@Override
+	public boolean validEntity(Object e) {
+		return e instanceof ThymioSharedEntity;
+	}
+
+}
