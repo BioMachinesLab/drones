007c3dab0b36873277a3892bf5e9525ddfc2ccb5
Vasco Craveiro Costa <atsoc0ocsav@gmail.com>
2015-05-20 15:56:00 +0100

    Recovered MapPanel

--- DroneControlConsole/src/gui/panels/map/MapPanel.java
+++ DroneControlConsole/src/gui/panels/map/MapPanel.java
@@ -1,13 +1,11 @@
 package gui.panels.map;
 
 import gui.panels.UpdatePanel;
-
 import java.awt.BasicStroke;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Cursor;
 import java.awt.Font;
-import java.awt.GridLayout;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -22,13 +20,11 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
-
-import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-
 import org.openstreetmap.gui.jmapviewer.Coordinate;
 import org.openstreetmap.gui.jmapviewer.JMapViewer;
 import org.openstreetmap.gui.jmapviewer.Layer;
@@ -43,11 +39,9 @@ import org.openstreetmap.gui.jmapviewer.interfaces.TileSource;
 import org.openstreetmap.gui.jmapviewer.tilesources.BingAerialTileSource;
 import org.openstreetmap.gui.jmapviewer.tilesources.MapQuestOsmTileSource;
 import org.openstreetmap.gui.jmapviewer.tilesources.OsmTileSource;
-
 import threads.UpdateThread;
 import commoninterface.entities.Entity;
 import commoninterface.entities.GeoFence;
-import commoninterface.entities.ObstacleLocation;
 import commoninterface.entities.RobotLocation;
 import commoninterface.entities.Waypoint;
 import commoninterface.network.messages.EntityMessage;
@@ -56,26 +50,29 @@ import commoninterface.utils.jcoord.LatLon;
 public class MapPanel extends UpdatePanel {
 	
 	private static final long serialVersionUID = 1L;
-	private static int OBSTACLE_RADIUS = 2;
-	private static int POSITION_HISTORY = 10;
 
     private JMapViewerTreeDrone treeMap = null;
+
+    private JButton fitMarkersButton;
+    
+    private static int POSITION_HISTORY = 10;
     
     private int robotMarkerIndex = 0;
     
     private HashMap<String,LinkedList<MapMarker>> robotPositions = new HashMap<String, LinkedList<MapMarker>>();
+    private LinkedList<MapMarker> waypointMarkers = new LinkedList<MapMarker>();
+    
+    private LinkedList<Waypoint> waypoints = new LinkedList<Waypoint>();
     private UpdateThread thread = null;
     
     private GeoFence geoFence = new GeoFence("geofence");
-    private LinkedList<Waypoint> waypoints = new LinkedList<Waypoint>();
-    private LinkedList<MapMarker> waypointMarkers = new LinkedList<MapMarker>();
-    private LinkedList<ObstacleLocation> obstacles = new LinkedList<ObstacleLocation>();
-    private LinkedList<MapMarker> obstacleMarkers = new LinkedList<MapMarker>();
-    
     private Layer geoFenceLayer;
     private boolean editingGeoFence = false;
-    private boolean addingObstacle = false;
 
+    /**
+     * Constructs the {@code Demo}.
+     * @throws MalformedURLException 
+     */
     public MapPanel() {
 
         treeMap = new JMapViewerTreeDrone("Zones");
@@ -83,16 +80,19 @@ public class MapPanel extends UpdatePanel {
         // final JMapViewer map = new JMapViewer(new MemoryTileCache(),4);
         // map.setTileLoader(new OsmFileCacheTileLoader(map));
         // new DefaultMapController(map);
-        setBorder(BorderFactory.createTitledBorder("Map"));
+
         setLayout(new BorderLayout());
-        JPanel panelTop = new JPanel(new GridLayout(2,3));
+        JPanel panel = new JPanel();
+        JPanel panelTop = new JPanel();
         JPanel helpPanel = new JPanel();
 
-        add(panelTop, BorderLayout.NORTH);
+        add(panel, BorderLayout.NORTH);
         add(helpPanel, BorderLayout.SOUTH);
+        panel.setLayout(new BorderLayout());
+        panel.add(panelTop, BorderLayout.NORTH);
         JLabel helpLabel = new JLabel("Left mouse: move // Double left click or mouse wheel: zoom // Right click: add markers");
         helpPanel.add(helpLabel);
-        JButton fitMarkersButton = new JButton("Fit Markers");
+        fitMarkersButton = new JButton("Fit Markers");
         fitMarkersButton.addActionListener(new ActionListener() {
 
             public void actionPerformed(ActionEvent e) {
@@ -125,6 +125,20 @@ public class MapPanel extends UpdatePanel {
         	e.printStackTrace();
         }
         
+        final JCheckBox showTreeLayers = new JCheckBox("Tree Layers");
+        showTreeLayers.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                treeMap.setTreeVisible(showTreeLayers.isSelected());
+            }
+        });
+        
+        JButton clearWaypointsButton = new JButton("Clear Waypoints");
+        clearWaypointsButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				clearWaypoints();
+			}
+		});
+        
         JButton geoFenceButton = new JButton(" Add GeoFence ");
         geoFenceButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -139,33 +153,10 @@ public class MapPanel extends UpdatePanel {
 			}
 		});
         
-        JButton clearWaypointsButton = new JButton("Clear Waypoints");
-        clearWaypointsButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				clearWaypoints();
-			}
-		});
-        
-        JButton addObstacleButton = new JButton("Add Obstacle");
-        addObstacleButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				addingObstacle = true;
-			}
-		});
-        
-        JButton clearObstaclesButton = new JButton("Clear Obstacles");
-        clearObstaclesButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				clearObstacles();
-			}
-		});
-        
-        panelTop.add(clearWaypointsButton);
-        panelTop.add(clearObstaclesButton);
+        panelTop.add(showTreeLayers);
         panelTop.add(fitMarkersButton);
         panelTop.add(geoFenceButton);
-        panelTop.add(addObstacleButton);        
-        
+        panelTop.add(clearWaypointsButton);
         add(treeMap, BorderLayout.CENTER);
         
         geoFenceLayer = treeMap.addLayer("_GeoFence");
@@ -178,12 +169,10 @@ public class MapPanel extends UpdatePanel {
             public void mouseClicked(MouseEvent e) {
             	
                if(e.getButton() == MouseEvent.BUTTON3) {
-            	   if(addingObstacle)
-            		   addObstacle(map().getPosition(e.getPoint()));
-            	   else if(editingGeoFence)
-            		   addToGeoFence(map().getPosition(e.getPoint()));
-            	   else
+            	   if(!editingGeoFence)
             		   addMarker(map().getPosition(e.getPoint()));
+            	   else
+            		   addToGeoFence(map().getPosition(e.getPoint()));
 //            	   updateRobotPosition("drone", map().getPosition(e.getPoint()));
                 }
             }
@@ -321,7 +310,7 @@ public class MapPanel extends UpdatePanel {
     	}
     	
     	if(robotMarkers.size() == 1 && robotPositions.size() == 1) {
-    		map().setDisplayToFitMapMarkers();
+    		fitMarkersButton.doClick();
     	}
     }
     
@@ -447,58 +436,13 @@ public class MapPanel extends UpdatePanel {
 	public LinkedList<Entity> getEntities() {
 		LinkedList<Entity> entities = new LinkedList<>();
 
-		if(!geoFence.getWaypoints().isEmpty())
+		if(!geoFence.getWaypoints().isEmpty()) {
 			entities.add(geoFence);
+		}
 		
 		if(waypoints != null)
 			entities.addAll(waypoints);
 		
-		if(obstacles != null)
-			entities.addAll(obstacles);
-		
 		return entities;
 	}
-	
-	private void addObstacle(Coordinate c) {
-		String layerName = "obstacles";
-    	
-    	Layer l = null;
-    	
-    	for(Layer layer : treeMap.getLayers())
-    		if(layer.getName().equals("obstacles"))
-    			l = layer;
-
-    	if(l == null) {
-    		l = treeMap.addLayer(layerName);
-    	}
-    	
-    	String markerName = "obstacle"+obstacles.size();
-    	
-    	MapMarker m = new MapMarkerObstacle(l, markerName , c);
-    	
-    	l.add(m);
-    	obstacleMarkers.add(m);
-    	
-    	map().addMapMarker(m);
-    	
-    	synchronized(this) {
-    		ObstacleLocation ol = new ObstacleLocation(markerName, new LatLon(c.getLat(),c.getLon()),OBSTACLE_RADIUS);
-    		obstacles.add(ol);
-//    		notifyAll();
-    	}
-		
-		addingObstacle = false;
-	}
-	
-	private void clearObstacles() {
-		
-		for(MapMarker m : obstacleMarkers) {
-	    	treeMap.removeFromLayer(m);
-	    	map().removeMapMarker(m);
-		}
-    	
-    	obstacles.clear();
-    	obstacleMarkers.clear();
-	}
-	
 }
