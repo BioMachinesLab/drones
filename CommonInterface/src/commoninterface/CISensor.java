ef8eec2c8d463a857453c38a03c95d4366826a60
Vasco Craveiro Costa <atsoc0ocsav@gmail.com>
2015-05-23 19:05:39 +0100

    Minnor Changes

--- CommonInterface/src/commoninterface/CISensor.java
+++ CommonInterface/src/commoninterface/CISensor.java
@@ -1,33 +1,32 @@
 package commoninterface;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 
-import commoninterface.entities.Entity;
 import commoninterface.utils.CIArguments;
 
-public abstract class CISensor implements Serializable{
-	
+public abstract class CISensor implements Serializable {
+
 	protected int id = 0;
 	protected RobotCI robot;
-	
+
 	public CISensor(int id, RobotCI robot, CIArguments args) {
 		super();
 		this.id = id;
 		this.robot = robot;
 	}
-	
+
 	public abstract int getNumberOfSensors();
-	
+
 	public abstract double getSensorReading(int sensorNumber);
-	
+
 	public abstract void update(double time, Object[] entities);
-	
-	public static CISensor getSensor(RobotCI robot, String name, CIArguments arguments) {
-		int id = arguments.getArgumentAsIntOrSetDefault("id",0);
-		return (CISensor)CIFactory.getInstance(name, id, robot, arguments);
+
+	public static CISensor getSensor(RobotCI robot, String name,
+			CIArguments arguments) {
+		int id = arguments.getArgumentAsIntOrSetDefault("id", 0);
+		return (CISensor) CIFactory.getInstance(name, id, robot, arguments);
 	}
-	
+
 	public int getId() {
 		return id;
 	}
