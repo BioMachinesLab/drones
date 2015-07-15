4cf8a201e0698bb9fa6793028245b24f1884e629
tiagorodrigues11 <tiagorodrigues11@gmail.com>
2015-07-15 18:06:04 +0100

    Broadcast address change from 255.255.255.255 to 192.168.3.255

--- RaspberryController/src/network/broadcast/BroadcastSender.java
+++ RaspberryController/src/network/broadcast/BroadcastSender.java
@@ -5,13 +5,18 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 
+import commoninterface.network.NetworkUtils;
+
 public class BroadcastSender {
 
 	private DatagramSocket socket;
 	private int port;
 
+	private String broacastAddress;
+	
 	public BroadcastSender(String ownAddress, int port) {
 		this.port = port;
+		broacastAddress = NetworkUtils.getBroadcastAddress(ownAddress);
 		try {
 			System.out.println("BroadcastSender own address is "+ownAddress);
 			socket = new DatagramSocket(port+1, InetAddress.getByName(ownAddress));
@@ -25,7 +30,7 @@ public class BroadcastSender {
 		try {
 			byte[] sendData = message.getBytes();
 			DatagramPacket sendPacket = new DatagramPacket(sendData,
-					sendData.length, InetAddress.getByName("255.255.255.255"),
+					sendData.length, InetAddress.getByName(broacastAddress),
 					port);
 			socket.send(sendPacket);
 		} catch (IOException e) {
