a89c70890d02cf4b2bd558cc9529b08b2e0839a5
Vasco Craveiro Costa <atsoc0ocsav@gmail.com>
2016-05-24 18:23:02 +0100

    Added normalization option to CompassCISensor

--- CommonInterface/src/commoninterface/sensors/CompassCISensor.java
+++ CommonInterface/src/commoninterface/sensors/CompassCISensor.java
@@ -1,33 +1,37 @@
 package commoninterface.sensors;
 
-import java.util.ArrayList;
-
 import commoninterface.AquaticDroneCI;
 import commoninterface.CISensor;
 import commoninterface.RobotCI;
-import commoninterface.entities.Entity;
 import commoninterface.utils.CIArguments;
 
-public class CompassCISensor extends CISensor{
-	
+public class CompassCISensor extends CISensor {
+
 	private AquaticDroneCI drone;
 	private double reading = 0;
+	private boolean normalize = false;
 
 	public CompassCISensor(int id, RobotCI robot, CIArguments args) {
 		super(id, robot, args);
 		drone = (AquaticDroneCI) robot;
+		normalize = args.getArgumentAsIntOrSetDefault("normalize", 0) == 1;
 	}
 
 	@Override
 	public double getSensorReading(int sensorNumber) {
-		return reading;
+		if (normalize) {
+			double val = reading % 360;
+			return val /= 360;
+		} else {
+			return reading;
+		}
 	}
 
 	@Override
 	public void update(double time, Object[] entities) {
-		reading = drone.getCompassOrientationInDegrees();	
+		reading = drone.getCompassOrientationInDegrees();
 	}
-	
+
 	@Override
 	public int getNumberOfSensors() {
 		return 1;
