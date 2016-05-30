bdaf1e2d2a58d8dd96ec81907e25fb87f9a540c5
Vasco Craveiro Costa <atsoc0ocsav@gmail.com>
2016-05-30 15:42:12 +0100

    Minor changes
    Changed the temperature units symbol and the line endings on make file
    and run script (windows messed up it :P)

--- RaspberryController/run.sh
+++ RaspberryController/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export CLASSPATH=/home/pi/RaspberryController/bin:/home/pi/CommonInterface/bin:/home/pi/Behaviors/bin:/home/pi/CommonInterface/lib/*:/opt/pi4j/lib/*:/home/pi/CommonInterface/jcoord-1.0.jar:.:*
+#export CLASSPATH=/home/pi/RaspberryController/bin:/home/pi/CommonInterface/bin:/home/pi/Behaviors/bin:/home/pi/CommonInterface/lib/*:/opt/pi4j/lib/*:/home/pi/CommonInterface/jcoord-1.0.jar:.:*
 make drone
