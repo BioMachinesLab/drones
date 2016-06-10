1f2bf3266c0dcdfa89922cbdb90157f3ec377378
Vasco Craveiro Costa <atsoc0ocsav@gmail.com>
2016-06-10 01:36:03 +0100

    Minor changes and feature addition

--- CommonInterface/src/commoninterface/entities/GeoFence.java
+++ CommonInterface/src/commoninterface/entities/GeoFence.java
@@ -10,62 +10,60 @@ import commoninterface.utils.logger.LogCodex;
 import commoninterface.utils.logger.LogCodex.LogType;
 
 public class GeoFence extends Entity {
-	
+	private static final long serialVersionUID = 4262661106635625238L;
 	private LinkedList<Waypoint> waypoints;
-	
+
 	public GeoFence(String name, LinkedList<Waypoint> waypoints) {
 		super(name);
 		this.waypoints = waypoints;
 	}
-	
+
 	public GeoFence(String name) {
 		super(name);
 		waypoints = new LinkedList<Waypoint>();
 	}
-	
+
 	public void addWaypoint(Waypoint wp) {
 		waypoints.add(wp);
 	}
-	
+
 	public void addWaypoint(LatLon latLon) {
-		Waypoint wp = new Waypoint("geofence_wp_"+waypoints.size(), latLon);
+		Waypoint wp = new Waypoint("geofence_wp_" + waypoints.size(), latLon);
 		waypoints.add(wp);
 	}
-	
+
 	public void clear() {
 		waypoints.clear();
 	}
-	
+
 	public LinkedList<Waypoint> getWaypoints() {
 		return waypoints;
 	}
-	
+
 	public static ArrayList<GeoFence> getGeoFences(AquaticDroneCI drone) {
 		ArrayList<GeoFence> geoFences = new ArrayList<GeoFence>();
-		
-		for(Entity e : drone.getEntities()) {
-			if(e instanceof GeoFence)
-				geoFences.add((GeoFence)e);
+
+		for (Entity e : drone.getEntities()) {
+			if (e instanceof GeoFence)
+				geoFences.add((GeoFence) e);
 		}
-		
+
 		return geoFences;
 	}
 
 	public String getLogMessage() {
 		ArrayList<Entity> entities = new ArrayList<Entity>(getWaypoints());
-		
+
 		return LogCodex.encodeLog(LogType.ENTITIES,
-				new EntityManipulation(
-						EntityManipulation.Operation.ADD, entities,
-						this.getClass().getSimpleName()));
+				new EntityManipulation(EntityManipulation.Operation.ADD, entities, this.getClass().getSimpleName()));
 	}
-	
+
 	@Override
 	public String toString() {
-		String s = this.getClass().getSimpleName()+";";
-		for(Waypoint w : getWaypoints())
-			s+= "FENCEWP;"+w.getLatLon().getLat()+";"+w.getLatLon().getLon()+";";
+		String s = this.getClass().getSimpleName() + ";";
+		for (Waypoint w : getWaypoints())
+			s += "FENCEWP;" + w.getLatLon().getLat() + ";" + w.getLatLon().getLon() + ";";
 		return s;
 	}
-	
+
 }
