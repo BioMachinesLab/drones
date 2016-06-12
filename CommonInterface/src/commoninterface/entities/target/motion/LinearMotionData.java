7b52d2111dbac88d69a9df14e364cd09ce12d462
Vasco Craveiro Costa <atsoc0ocsav@gmail.com>
2016-06-12 19:29:08 +0100

    Implemented MotionData superclass and rotation and linear motion types

---
+++ CommonInterface/src/commoninterface/entities/target/motion/LinearMotionData.java
@@ -0,0 +1,80 @@
+package commoninterface.entities.target.motion;
+
+import commoninterface.entities.GeoEntity;
+import commoninterface.mathutils.Vector2d;
+import commoninterface.utils.CoordinateUtilities;
+import commoninterface.utils.jcoord.LatLon;
+
+public class LinearMotionData extends MotionData {
+	private boolean move = true;
+
+	private double movementVelocity;
+	private double movementAzimuth;
+
+	public LinearMotionData(GeoEntity entity, LatLon originalPosition, double movementAzimuth,
+			double movementVelocity) {
+		super(entity, MovementType.LINEAR);
+
+		this.movementAzimuth = movementAzimuth;
+		this.movementVelocity = movementVelocity;
+
+		velocityVector.setPolarCoordinates(movementVelocity, movementAzimuth);
+	}
+
+	@Override
+	public LatLon calculatePosition(double step) {
+		Vector2d currentPosition = new Vector2d(originalPositionCartesian);
+		currentPosition.add(calculateTranslationVector(step));
+		return CoordinateUtilities.cartesianToGPS(currentPosition);
+	}
+
+	@Override
+	public Vector2d calculateTranslationVector(double step) {
+		double walkedDistance = 0;
+		double azimuth = getVelocityVector(0).getAngle();
+
+		for (int i = 0; i < step; i++) {
+			walkedDistance = (getVelocityVector(step).length()) / UPDATE_RATE;
+		}
+
+		Vector2d walkedVector = new Vector2d(0, 0);
+		walkedVector.setPolarCoordinates(walkedDistance, azimuth);
+		return walkedVector;
+	}
+
+	/*
+	 * Getters and setters
+	 */
+	@Override
+	public Vector2d getVelocityVector(double step) {
+		if (move) {
+			return super.getVelocityVector(step);
+		} else {
+			return new Vector2d(0, 0);
+		}
+	}
+
+	public GeoEntity getEntity() {
+		return entity;
+	}
+
+	public void setMove(boolean move) {
+		this.move = move;
+	}
+
+	public double getMovementVelocity() {
+		return movementVelocity;
+	}
+
+	public double getMovementAzimuth() {
+		return movementAzimuth;
+	}
+
+	public void setMovementAzimuth(double targetMovementAzimuth) {
+		this.movementAzimuth = targetMovementAzimuth;
+	}
+
+	public void setMovementVelocity(double targetMovementVelocity) {
+		this.movementVelocity = targetMovementVelocity;
+	}
+}
---
+++ CommonInterface/src/commoninterface/entities/target/motion/MotionData.java
@@ -0,0 +1,45 @@
+package commoninterface.entities.target.motion;
+
+import commoninterface.entities.GeoEntity;
+import commoninterface.mathutils.Vector2d;
+import commoninterface.utils.CoordinateUtilities;
+import commoninterface.utils.jcoord.LatLon;
+
+public abstract class MotionData {
+	public static enum MovementType {
+		LINEAR, ROTATIONAL;
+	}
+
+	protected static final int UPDATE_RATE = 10; // Assuming 100ms simulation
+													// steps
+
+	protected LatLon originalPosition;
+	protected GeoEntity entity;
+	protected Vector2d originalPositionCartesian;
+	protected MovementType motionType;
+	protected Vector2d velocityVector;
+
+	public MotionData(GeoEntity entity, MovementType motionType) {
+		this.entity = entity;
+		this.originalPosition = entity.getLatLon();
+		originalPositionCartesian = CoordinateUtilities.GPSToCartesian(entity.getLatLon());
+		this.motionType = motionType;
+		velocityVector = new Vector2d(0, 0);
+	}
+
+	public MovementType getMotionType() {
+		return motionType;
+	}
+
+	public Vector2d getVelocityVector(double step) {
+		return velocityVector;
+	}
+
+	public LatLon getOriginalPosition() {
+		return originalPosition;
+	}
+
+	public abstract LatLon calculatePosition(double step);
+
+	public abstract Vector2d calculateTranslationVector(double step);
+}
