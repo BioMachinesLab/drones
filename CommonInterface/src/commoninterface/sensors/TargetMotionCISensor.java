6a6e1f2578e975fe0fa8728844c3e53874452076
Vasco Craveiro Costa <atsoc0ocsav@gmail.com>
2016-06-13 06:20:30 +0100

    Change to cope with new motion system

--- CommonInterface/src/commoninterface/sensors/TargetMotionCISensor.java
+++ CommonInterface/src/commoninterface/sensors/TargetMotionCISensor.java
@@ -20,10 +20,6 @@ public class TargetMotionCISensor extends CISensor {
 	private boolean stabilize = false;
 	private boolean normalize = false;
 
-	// XXX
-	private boolean alternative = false;
-	private boolean vascos = false;
-
 	private double range = 100;
 	private int historySize = 10;
 	private Target[] lastSeenTargets;
@@ -38,10 +34,6 @@ public class TargetMotionCISensor extends CISensor {
 		range = args.getArgumentAsDoubleOrSetDefault("range", range);
 		normalize = args.getArgumentAsIntOrSetDefault("normalize", 0) == 1;
 
-		// XXX
-		alternative = args.getArgumentAsIntOrSetDefault("normalize", 0) == 1;
-		vascos = args.getArgumentAsIntOrSetDefault("vascos", 0) == 1;
-
 		lastSeenTargets = new Target[historySize];
 		for (int i = 0; i < lastSeenTargets.length; i++) {
 			lastSeenTargets[i] = null;
@@ -51,42 +43,33 @@ public class TargetMotionCISensor extends CISensor {
 	@Override
 	public void update(double time, Object[] entities) {
 		Target target = getClosestTarget(excludeOccupied, entities);
-		if (vascos) {
-			if (target != null) {
-				Vector2d vel = target.getTargetMotionData().getVelocityVector(time);
-				readings[0] = vel.getAngle() / Math.PI / 2;
-				readings[1] = vel.length();
+		if (target != null) {
+			Vector2d vel = target.getTargetMotionData().getVelocityVector(time);
+			readings[0] = vel.getAngle() / Math.PI / 2;
+			readings[1] = vel.length();
 
-				// Move the readings to the positive side of the scale
-				if (readings[0] < 0) {
-					readings[0] += FastMath.PI * 2;
-				}
-				readings[0] %= FastMath.PI * 2;
+			// Move the readings to the positive side of the scale
+			if (readings[0] < 0) {
+				readings[0] += FastMath.PI * 2;
+			}
+			readings[0] %= FastMath.PI * 2;
 
-				// Normalization and detection range
-				if (robot instanceof AquaticDroneCI) {
-					// Is the sensed target inside the sensor range?
-					double distance = ((AquaticDroneCI) robot).getGPSLatLon().distanceInMeters(target.getLatLon());
-					if (target.getTargetMotionData() instanceof LinearMotionData && distance < range) {
-						if (normalize) {
-							readings[0] /= (FastMath.PI * 2);
-						}
-						// In case the target is outside the range
-					} else {
-						readings[0] = 0;
-						readings[1] = 0;
+			// Normalization and detection range
+			if (robot instanceof AquaticDroneCI) {
+				// Is the sensed target inside the sensor range?
+				double distance = ((AquaticDroneCI) robot).getGPSLatLon().distanceInMeters(target.getLatLon());
+				if (target.getTargetMotionData() instanceof LinearMotionData && distance < range) {
+					if (normalize) {
+						readings[0] /= (FastMath.PI * 2);
 					}
+					// In case the target is outside the range
 				} else {
 					readings[0] = 0;
 					readings[1] = 0;
 				}
-			}
-		} else {
-			// Sancho's version
-			if (target != null) {
-				Vector2d vel = target.getTargetMotionData().getVelocityVector(time);
-				readings[0] = vel.length();
-				readings[1] = (vel.getAngle() + Math.PI) / Math.PI / 2;
+			} else {
+				readings[0] = 0;
+				readings[1] = 0;
 			}
 		}
 	}
@@ -116,25 +99,14 @@ public class TargetMotionCISensor extends CISensor {
 		for (Object ent : ((AquaticDroneCI) robot).getEntities()) {
 			if (ent instanceof Target) {
 				Vector2d pos = CoordinateUtilities.GPSToCartesian(((Target) ent).getLatLon());
-
-				// XXX
-				if (alternative) {
-					if (((Target) ent).isOccupied() && robotPosition.distanceTo(pos) <= ((Target) ent).getRadius()
-							&& robotPosition.distanceTo(pos) < minDistance) {
+				if (((Target) ent).isOccupied() && robotPosition.distanceTo(pos) <= ((Target) ent).getRadius()
+						&& robotPosition.distanceTo(pos) < minDistance) {
+					minDistance = robotPosition.distanceTo(pos);
+					closest = (Target) ent;
+				} else {
+					if (!((Target) ent).isOccupied() && robotPosition.distanceTo(pos) < minDistance) {
 						minDistance = robotPosition.distanceTo(pos);
 						closest = (Target) ent;
-					} else {
-						if (!((Target) ent).isOccupied() && robotPosition.distanceTo(pos) < minDistance) {
-							minDistance = robotPosition.distanceTo(pos);
-							closest = (Target) ent;
-						}
-					}
-				} else {
-					if (!excludeOccupied || (excludeOccupied && !((Target) ent).isOccupied())) {
-						if (robotPosition.distanceTo(pos) < minDistance) {
-							minDistance = robotPosition.distanceTo(pos);
-							closest = (Target) ent;
-						}
 					}
 				}
 			}
