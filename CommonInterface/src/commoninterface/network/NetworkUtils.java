1e1cf5f4e13da6b760ccb08a3c72c254d2380df3
Vasco Craveiro Costa <atsoc0ocsav@gmail.com>
2016-06-14 23:00:19 +0100

    Added one more address mask

--- CommonInterface/src/commoninterface/network/NetworkUtils.java
+++ CommonInterface/src/commoninterface/network/NetworkUtils.java
@@ -12,22 +12,22 @@ public class NetworkUtils {
 
 	// In order to find our own IP address among all possible NICs,
 	// we should at least know how it starts.
-	private static final String ADDRESS_START1 = "192.168.3";
+	// private static final String ADDRESS_START1 = "192.168.3";
+	private static final String ADDRESS_START1 = "192.168.1";
 	private static final String ADDRESS_START2 = "172.17.";
+	private static final String ADDRESS_START3 = "192.168.3";
 
-	
 	public static String getAddress() {
 		if (ADDRESS == null) {
 			try {
-				for (Enumeration<NetworkInterface> en = NetworkInterface
-						.getNetworkInterfaces(); en.hasMoreElements();) {
+				for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en
+						.hasMoreElements();) {
 					NetworkInterface intf = en.nextElement();
 
-					for (Enumeration<InetAddress> enumIpAddr = intf
-							.getInetAddresses(); enumIpAddr.hasMoreElements();) {
-						String next = enumIpAddr.nextElement().toString()
-								.replace("/", "");
-						if (next.startsWith(ADDRESS_START1) || next.startsWith(ADDRESS_START2)) {
+					for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements();) {
+						String next = enumIpAddr.nextElement().toString().replace("/", "");
+						if (next.startsWith(ADDRESS_START1) || next.startsWith(ADDRESS_START2)
+								|| next.startsWith(ADDRESS_START3)) {
 							ADDRESS = next;
 							return next;
 						}
@@ -46,18 +46,17 @@ public class NetworkUtils {
 
 		if (ADDRESS == null) {
 			try {
-				for (Enumeration<NetworkInterface> en = NetworkInterface
-						.getNetworkInterfaces(); en.hasMoreElements();) {
+				for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en
+						.hasMoreElements();) {
 					NetworkInterface intf = en.nextElement();
 
 					if (intf.getName().equals(intfName)) {
 
-						for (Enumeration<InetAddress> enumIpAddr = intf
-								.getInetAddresses(); enumIpAddr
+						for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr
 								.hasMoreElements();) {
-							String next = enumIpAddr.nextElement().toString()
-									.replace("/", "");
-							if (next.startsWith(ADDRESS_START1) || next.startsWith(ADDRESS_START2)) {
+							String next = enumIpAddr.nextElement().toString().replace("/", "");
+							if (next.startsWith(ADDRESS_START1) || next.startsWith(ADDRESS_START2)
+									|| next.startsWith(ADDRESS_START3)) {
 								ADDRESS = next;
 								return next;
 							}
@@ -75,15 +74,15 @@ public class NetworkUtils {
 
 	public static String getBroadcastAddress(String address) {
 		String broadcastAddress = "";
-		
+
 		String[] split = address.split("\\.");
-		
-		for (int i = 0; i < split.length-1; i++)
+
+		for (int i = 0; i < split.length - 1; i++)
 			broadcastAddress += split[i] + ".";
-		
+
 		return broadcastAddress + "255";
 	}
-	
+
 	public static String getHostname() {
 		try {
 			return InetAddress.getLocalHost().getHostName();
