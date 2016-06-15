da5b3d58e3e910155509d0b278504591538a14ef
Vasco Craveiro Costa <atsoc0ocsav@gmail.com>
2016-06-15 04:22:41 +0100

    Implemented motion provider for formation

--- CommonInterface/src/commoninterface/messageproviders/EntitiesMessageProvider.java
+++ CommonInterface/src/commoninterface/messageproviders/EntitiesMessageProvider.java
@@ -19,74 +19,75 @@ import commoninterface.utils.logger.EntityManipulation.Operation;
 import commoninterface.utils.logger.LogCodex;
 import commoninterface.utils.logger.LogCodex.LogType;
 
-public class EntitiesMessageProvider implements MessageProvider{
-	
+public class EntitiesMessageProvider implements MessageProvider {
+
 	private RobotCI robot;
-	
+
 	public EntitiesMessageProvider(RobotCI drone) {
 		this.robot = drone;
 	}
 
 	@Override
 	public Message getMessage(Message m) {
-		
-		if(m instanceof EntitiesMessage) {
-			
-			EntitiesMessage wm = (EntitiesMessage)m;
+
+		if (m instanceof EntitiesMessage) {
+
+			EntitiesMessage wm = (EntitiesMessage) m;
 			ArrayList<Entity> entities = wm.getEntities();
-			
-			synchronized(robot.getEntities()) {
-			
+
+			synchronized (robot.getEntities()) {
+
 				Iterator<Entity> i = robot.getEntities().iterator();
-				
+
 				ArrayList<Entity> entitiesToRemove = new ArrayList<Entity>();
-				while(i.hasNext()) {
+				while (i.hasNext()) {
 					Entity e = i.next();
-					System.out.println("REMOVING "+e.getClass().getSimpleName());
-					if(e instanceof GeoFence || e instanceof Waypoint || e instanceof ObstacleLocation) {
+					System.out.println("REMOVING " + e.getClass().getSimpleName());
+					if (e instanceof GeoFence || e instanceof Waypoint || e instanceof ObstacleLocation
+							|| e instanceof Formation) {
 						entitiesToRemove.add(e);
 						i.remove();
 					}
 				}
-				
+
 				if (!entitiesToRemove.isEmpty()) {
-					EntityManipulation msg = new EntityManipulation(
-							Operation.REMOVE, entitiesToRemove, GeoEntity.class.getSimpleName());
+					EntityManipulation msg = new EntityManipulation(Operation.REMOVE, entitiesToRemove,
+							GeoEntity.class.getSimpleName());
 					log(LogCodex.encodeLog(LogType.ENTITIES, msg));
 				}
-				
+
 				robot.getEntities().addAll(entities);
 			}
-			
-			for(Entity e : entities) {
-				System.out.println("ADDED "+e.getClass().getSimpleName());
-				if(e instanceof GeoEntity) {
-					GeoEntity ge = (GeoEntity)e;
+
+			for (Entity e : entities) {
+				System.out.println("ADDED " + e.getClass().getSimpleName());
+				if (e instanceof GeoEntity) {
+					GeoEntity ge = (GeoEntity) e;
 					log(ge.getLogMessage());
-				} else if(e instanceof GeoFence) {
-					GeoFence gf = (GeoFence)e;
+				} else if (e instanceof GeoFence) {
+					GeoFence gf = (GeoFence) e;
 					log(gf.getLogMessage());
-				}else if (e instanceof Formation){
-					Formation gf = (Formation)e;
+				} else if (e instanceof Formation) {
+					Formation gf = (Formation) e;
 					log(gf.getLogMessage());
 				}
 			}
-			
-			if(robot instanceof AquaticDroneCI) {
+
+			if (robot instanceof AquaticDroneCI) {
 				ArrayList<Waypoint> wps = Waypoint.getWaypoints(robot);
-				
-				if(!wps.isEmpty()) {
+
+				if (!wps.isEmpty()) {
 					((AquaticDroneCI) robot).setActiveWaypoint(wps.get(wm.getActiveId()));
-					System.out.println("Active waypoint is "+wm.getActiveId());
+					System.out.println("Active waypoint is " + wm.getActiveId());
 				}
 			}
 			return m;
 		}
 		return null;
 	}
-	
+
 	private void log(String msg) {
-		if(robot.getLogger() != null){
+		if (robot.getLogger() != null) {
 			robot.getLogger().logMessage(msg);
 		}
 	}
