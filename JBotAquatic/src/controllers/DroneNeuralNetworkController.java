52241518b12544b71ada2f7ce9f4346a9ce4d42e
Vasco Craveiro Costa <atsoc0ocsav@gmail.com>
2016-11-18 20:20:14 +0000

    Minor changes

--- JBotAquatic/src/controllers/DroneNeuralNetworkController.java
+++ JBotAquatic/src/controllers/DroneNeuralNetworkController.java
@@ -16,33 +16,36 @@ public class DroneNeuralNetworkController extends Controller implements FixedLen
 	private static final long serialVersionUID = -6938294613191709589L;
 
 	protected CINeuralNetwork neuralNetwork;
-	
-	@ArgumentsAnnotation(name="printweights", values={"0","1"})
+
+	@ArgumentsAnnotation(name = "printweights", values = { "0", "1" })
 	protected boolean printWeights = false;
 
 	public DroneNeuralNetworkController(Simulator simulator, Robot robot, Arguments args) {
 		super(simulator, robot, args);
-		
-		neuralNetwork = CINeuralNetwork.getNeuralNetwork(((RobotCI) robot), new CIArguments(args.getArgumentAsString("network")));
-		
-		for(Sensor s : robot.getSensors()) {
-			if(s.getNumberExtraParameters() > 0)
-				neuralNetwork.setRequiredNumberOfWeights(neuralNetwork.getGenomeLength() + s.getNumberExtraParameters());
+
+		neuralNetwork = CINeuralNetwork.getNeuralNetwork(((RobotCI) robot),
+				new CIArguments(args.getArgumentAsString("network")));
+
+		for (Sensor s : robot.getSensors()) {
+			if (s.getNumberExtraParameters() > 0)
+				neuralNetwork
+						.setRequiredNumberOfWeights(neuralNetwork.getGenomeLength() + s.getNumberExtraParameters());
 		}
-		
-		for(Actuator a : robot.getActuators()) {
-			if(a.getNumberExtraParameters() > 0)
-				neuralNetwork.setRequiredNumberOfWeights(neuralNetwork.getGenomeLength() + a.getNumberExtraParameters());
+
+		for (Actuator a : robot.getActuators()) {
+			if (a.getNumberExtraParameters() > 0)
+				neuralNetwork
+						.setRequiredNumberOfWeights(neuralNetwork.getGenomeLength() + a.getNumberExtraParameters());
 		}
-		
-		if(args.getArgumentIsDefined("weights")) {
+
+		if (args.getArgumentIsDefined("weights")) {
 			String[] rawArray = args.getArgumentAsString("weights").split(",");
 			double[] weights = new double[rawArray.length];
-			for(int i = 0 ; i < weights.length ; i++)
+			for (int i = 0; i < weights.length; i++)
 				weights[i] = Double.parseDouble(rawArray[i]);
 			setNNWeights(weights);
 		}
-		
+
 		printWeights = args.getArgumentAsIntOrSetDefault("printweights", 0) == 1;
 	}
 
@@ -67,7 +70,7 @@ public class DroneNeuralNetworkController extends Controller implements FixedLen
 	public int getGenomeLength() {
 		return neuralNetwork.getGenomeLength();
 	}
-	
+
 	public void setNeuralNetwork(CINeuralNetwork neuralNetwork) {
 		this.neuralNetwork = neuralNetwork;
 	}
@@ -84,42 +87,42 @@ public class DroneNeuralNetworkController extends Controller implements FixedLen
 
 	@Override
 	public void setNNWeights(double[] weights) {
-		
-		if(printWeights) {
-			String w = "#weights (total of "+weights.length+")\n";
-			for(int i = 0 ; i < weights.length ; i++) {
-				w+=weights[i];
-				if(i != weights.length-1)
-					w+=",";
+
+		if (printWeights) {
+			String w = "#weights (total of " + weights.length + ")\n";
+			for (int i = 0; i < weights.length; i++) {
+				w += weights[i];
+				if (i != weights.length - 1)
+					w += ",";
 			}
 			System.out.println(w);
 		}
-		
+
 		neuralNetwork.setWeights(weights);
 
 		int currentIndex = weights.length - 1;
-		
-		for(Sensor s : robot.getSensors()) {
-			if(s.getNumberExtraParameters() > 0) {
+
+		for (Sensor s : robot.getSensors()) {
+			if (s.getNumberExtraParameters() > 0) {
 				double[] params = new double[s.getNumberExtraParameters()];
-				for(int i = 0 ; i < params.length ; i++,currentIndex--) {
+				for (int i = 0; i < params.length; i++, currentIndex--) {
 					params[i] = weights[currentIndex];
 				}
 				s.setExtraParameters(params);
 			}
 		}
-		
-		for(Actuator a : robot.getActuators()) {
-			if(a.getNumberExtraParameters() > 0) {
+
+		for (Actuator a : robot.getActuators()) {
+			if (a.getNumberExtraParameters() > 0) {
 				double[] params = new double[a.getNumberExtraParameters()];
-				for(int i = 0 ; i < params.length ; i++,currentIndex--) {
+				for (int i = 0; i < params.length; i++, currentIndex--) {
 					params[i] = weights[currentIndex];
 				}
 				a.setExtraParameters(params);
 			}
 		}
 	}
-	
+
 	@Override
 	public double[] getNNWeights() {
 		return neuralNetwork.getWeights();
@@ -127,22 +130,22 @@ public class DroneNeuralNetworkController extends Controller implements FixedLen
 
 	public static void setNNWeights(LinkedList<Robot> robots, double[] weights) {
 		for (Robot r : robots) {
-			if (r.getController() instanceof FixedLenghtGenomeEvolvableController){
-				FixedLenghtGenomeEvolvableController nnController = (FixedLenghtGenomeEvolvableController) r.getController();
+			if (r.getController() instanceof FixedLenghtGenomeEvolvableController) {
+				FixedLenghtGenomeEvolvableController nnController = (FixedLenghtGenomeEvolvableController) r
+						.getController();
 				if (nnController != null)
 					nnController.setNNWeights(weights);
 			}
 		}
 	}
-	
+
 	@Override
 	public int getNumberOfInputs() {
 		return neuralNetwork.getNumberOfInputNeurons();
 	}
-	
+
 	@Override
 	public int getNumberOfOutputs() {
 		return neuralNetwork.getNumberOfOutputNeurons();
 	}
-
 }
